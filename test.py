0e4e24fc66c57e2f3573bdc7546a0ada34d331c0
jaime <jaime.cernuda95@gmail.com>
2019-04-18 23:50:21 -0500

    Minor improvements. Change image load to keras

--- test.py
+++ test.py
@@ -5,19 +5,20 @@ from keras.callbacks import CSVLogger, ModelCheckpoint, ReduceLROnPlateau
 from keras.layers import Dense, GlobalAveragePooling2D
 from keras.utils import Sequence
 from skimage import transform
+from keras.preprocessing import image
 
 from keras_applications.imagenet_utils import preprocess_input
 from keras import optimizers
 import sklearn
 import random
 import numpy as np
-import pandas as pd
+import os
 
 
 class MuraGenerator(Sequence):
     def __init__(self, path_to_file, batch_size, shuffle=True):
         self.bs = batch_size
-        self.paths_images = pd.read_csv(path_to_file, names=["paths"])
+        self.paths_images = np.genfromtxt(path_to_file, delimiter=',')
         self.shuffle = shuffle
         self.on_epoch_end()
 
@@ -29,25 +30,25 @@ class MuraGenerator(Sequence):
         y_batch = []
 
         while len(x_batch) < self.bs:
-            path = str(self.paths_images['paths'].iloc[idx])
-            image = imageio.imread(path, as_gray=False)
+            path = self.paths_images[idx]
+            img = image.load_img(path, color_mode='rgb',
+                                 target_size=(320, 320))
 
-            image = self.transform_image(image)
+            img = image.img_to_array(img)
+            # img = np.expand_dims(img, -1)
+            print(img.shape)
+            img = self.transform_image(img)
 
-            x_batch.append(image)
-            if "positive" in path:
-                y_batch.append(1)
-            else:
-                y_batch.append(0)
+            x_batch.append(img)
+            y_batch.append(1 if "positive" in path else 0)
 
         return [np.asarray(x_batch), np.asarray(y_batch)]
 
     def on_epoch_end(self):
-        if self.shuffle is True:
-            self.paths_images = sklearn.utils.shuffle(self.paths_images)
+        if self.shuffle:
+            self.paths_images = random.shuffle(self.paths_images)
 
     def transform_image(self, image):
-        image = transform.resize(image, (320, 320))
 
         # image = preprocess_input(image)
 
@@ -74,9 +75,9 @@ def generate_model():
     model.add(Dense(1, activation='sigmoid'))
     model.summary()
 
-    Adam = optimizers.Adam(lr=0.0001)
+    adam = optimizers.Adam(lr=0.0001)
 
-    model.compile(optimizer=Adam,
+    model.compile(optimizer=adam,
                   metrics=['accuracy'],
                   loss='binary_crossentropy')
 
@@ -84,8 +85,12 @@ def generate_model():
 
 
 if __name__ == "__main__":
-    train_generator = MuraGenerator(path_to_file='MURA-v1.1/train_image_paths.csv', batch_size=64)
+    train_generator = MuraGenerator(
+        path_to_file='MURA-v1.1/train_image_paths.csv', batch_size=64)
     model = generate_model()
+
+    if not os.path.isdir('logs'):
+        os.mkdir('logs')
 
     csvlogger = CSVLogger('logs/training.log')
     checkpointer = ModelCheckpoint('models/model.hdf5', save_best_only=True)
@@ -93,5 +98,6 @@ if __name__ == "__main__":
                                   patience=2, min_lr=1e-6)
 
     model.fit_generator(train_generator,
+                        verbose=1,
                         callbacks=[csvlogger, checkpointer, reduce_lr],
                         class_weight={0: 0.3848, 1: 0.6152})
