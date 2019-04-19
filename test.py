203ffaad2d239cdfd118f5c79ec653cb40403960
jaime <jaime.cernuda95@gmail.com>
2019-04-19 00:15:27 -0500

    print

--- test.py
+++ test.py
@@ -19,6 +19,7 @@ class MuraGenerator(Sequence):
     def __init__(self, path_to_file, batch_size, shuffle=True):
         self.bs = batch_size
         self.paths_images = np.loadtxt(path_to_file, dtype='str')
+        print(str(self.paths_images[0]))
         self.shuffle = shuffle
         self.on_epoch_end()
 
