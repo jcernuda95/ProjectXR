f52ae57e2553b8d9dcbf156ca0695655a2b4b22c
jaime <jaime.cernuda95@gmail.com>
2019-04-19 20:30:55 -0500

    no wieghts

--- test.py
+++ test.py
@@ -21,17 +21,17 @@ from sklearn.metrics import precision_score, recall_score
 
 def recall(y_true, y_pred):
     weights = K.tf.convert_to_tensor([0.5959302325581395, 0.40406976744186046], dtype=K.tf.float32)
-    return K.tf.metrics.recall(y_true, y_pred, weights)
+    return K.tf.metrics.recall(y_true, y_pred)
 
 
 def precision(y_true, y_pred):
     weights = K.tf.convert_to_tensor([0.5959302325581395, 0.40406976744186046], dtype=K.tf.float32)
-    return K.tf.metrics.precision(y_true, y_pred, weights)
+    return K.tf.metrics.precision(y_true, y_pred)
 
 
 def auc(y_true, y_pred):
     weights = K.tf.convert_to_tensor([0.5959302325581395, 0.40406976744186046], dtype=K.tf.float32)
-    return K.tf.metrics.auc(y_true, y_pred, weights)
+    return K.tf.metrics.auc(y_true, y_pred)
 
 
 class MuraGenerator(Sequence):
