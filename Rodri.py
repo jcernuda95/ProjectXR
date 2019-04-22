64e1d5760950716935f7cfc05b6ee20b4a5436e1
Jaime Cernuda <jcernuda@comet-ln2.sdsc.edu>
2019-04-22 15:32:33 -0700

    model

---
+++ Rodri.py
@@ -0,0 +1,383 @@
+import argparse
+import os
+import random
+
+from glob import glob
+
+import matplotlib.pyplot as plt
+from keras.models import Model
+
+import numpy as np
+from keras import Sequential
+from keras import backend as K
+from keras import regularizers
+from keras import optimizers
+from keras import initializers
+from keras.metrics import binary_accuracy, binary_crossentropy
+from keras.applications import DenseNet169, MobileNet
+from keras.callbacks import CSVLogger, ModelCheckpoint, ReduceLROnPlateau
+from keras.layers import Dense, GlobalAveragePooling2D, Flatten
+from keras.preprocessing import image
+from keras.utils import Sequence
+# from keras_applications.densenet import preprocess_input
+from skimage import transform
+from sklearn.metrics import roc_auc_score, precision_score, recall_score, log_loss, accuracy_score
+from sklearn.model_selection import train_test_split
+
+import pandas as pd
+
+
+def preprocess_input(img, data_format):
+    img /= 255.
+    mean = [0.485, 0.456, 0.406]
+    std = [0.229, 0.224, 0.225]
+    img[..., 0] -= mean[0]
+    img[..., 1] -= mean[1]
+    img[..., 2] -= mean[2]
+
+    img[..., 0] /= std[0]
+    img[..., 1] /= std[1]
+    img[..., 2] /= std[2]
+    return img
+
+
+def recall(y_true, y_pred, weights):
+    return recall_score(y_true, y_pred, sample_weight=weights)
+
+
+def precision(y_true, y_pred, weights):
+    return precision_score(y_true, y_pred, sample_weight=weights)
+
+
+def auc(y_true, y_pred, weights):
+    return roc_auc_score(y_true, y_pred, sample_weight=weights)
+
+
+def transform_image(image, augment):
+    # image = preprocess_input(image, data_format='channels_last')
+
+    image *= 1./255
+
+    if augment:
+        if random.randint(0, 1):
+            image = np.fliplr(image)
+
+        angle = random.randint(-30, 30)
+        image = transform.rotate(image, angle)
+
+    return image
+
+
+def rgb2gray(rgb):
+    return np.dot(rgb[..., :3], [0.2989, 0.5870, 0.1140])
+
+
+def get_cam(model, img, predicted_class):
+    output = model.output
+
+    densenet_layer = model.get_layer('densenet169')
+    final_conv_layer = densenet_layer.get_layer('relu')
+
+    grads = K.gradients(model.output,
+                        model.get_layer('densenet169').layers[-1].output)[0]
+
+    print(grads)
+
+    pooled = K.mean(grads, axis=(0, 1, 2))
+
+    print(pooled)
+    iterate = K.function([model.input], [pooled, final_conv_layer.output[0]])
+
+    pooled_val, final_conv_val = iterate(img)
+
+    depth = final_conv_layer.shape[-1]
+
+    for i in range(depth):
+        final_conv_layer[:, :, i] *= pooled_val[i]
+
+    heatmap = np.mean(conv_layer_output_value, axis=-1)
+    heatmap = np.maximum(heatmap, 0)
+    heatmap /= np.max(heatmap)
+
+    return heatmap
+
+
+class MuraGenerator(Sequence):
+    def __init__(self, paths_studies, batch_size, weights, augment=True):
+        self.bs = batch_size
+        self.paths_studies = paths_studies
+        self.augment = augment
+        self.weights = weights
+
+    def __len__(self):
+        return len(self.paths_studies) // self.bs
+
+    def __getitem__(self, idx):
+        x_batch = []
+        y_batch = []
+        w_batch = []
+
+        paths = self.paths_studies[idx * self.bs: (idx + 1) * self.bs]
+        for path in paths:
+            section = path[0][16:23]
+            img_paths = glob(str(path[0]) + '*')
+            for img_path in img_paths:
+                img = image.load_img(img_path, color_mode='rgb',
+                                     target_size=(320, 320))
+
+                img = image.img_to_array(img)
+
+                img = transform_image(img, self.augment)
+
+                x_batch.append(img)
+                y_batch.append(int(path[1]))
+                w_batch.append(self.weights[section][int(path[1])])
+
+        return [np.asarray(x_batch), np.asarray(y_batch), np.asarray(w_batch)]
+
+
+def generate_model(stage):
+    K.clear_session()
+    base_model = DenseNet169(include_top=False,
+                             input_shape=(320, 320, 3),
+                             weights='imagenet')
+
+    x = base_model.output
+    x = GlobalAveragePooling2D()(x)
+    x = Dense(1, activation='sigmoid', name='predictions')(x)
+
+    model = Model(inputs=base_model.inputs, outputs=x)
+
+    adam = optimizers.Adam(lr=1e-4)
+    sgd = optimizers.SGD(lr=1e-4)
+
+    for layer in base_model.layers:
+        layer.trainable = False
+
+        
+    model.compile(optimizer=sgd,
+                  metrics=['binary_accuracy'],
+                  loss='binary_crossentropy')
+
+    if stage >= 2:
+        if args.model_path:
+            model.load_weights(args.model_path, by_name=True)
+            print("Loaded from: ", args.model_path)
+
+        if stage == 2:
+            set_trainable = False
+            for layer in base_model.layers:
+                # if "block12" in layer.name:  # what block do we want to start unfreezing
+                set_trainable = True
+                if set_trainable:
+                    layer.trainable = True
+                else:
+                    layer.trainable = False
+
+    model.compile(optimizer=sgd,
+                  metrics=['binary_accuracy'],
+                  loss='binary_crossentropy')
+
+    if args.print_summary == True:
+        model.summary()
+
+    return model
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="MURA image classification")
+    parser.add_argument('-r', '--resume', action='store_true', default='False',
+                        help='Resume training from last saved model')
+    parser.add_argument('-s', '--stage', default=0, type=int,
+                        help='Set stage of training: '
+                             '0-train only dense layer'
+                             '1-train only dense layer with image augmentation.'
+                             '2-train dense with augmentation and last conv block.'
+                             '3-testing, report all metric of the test data.'
+                             '4-evaluate a single client, indicated with -c, plot image and CAM.')
+    parser.add_argument('--train_path', default='./MURA-v1.1/train_labeled_studies.csv',
+                        help='Path to train_labeled_studies.csv')
+    parser.add_argument('--train_images', default='./MURA-v1.1/train_image_paths.csv',
+                        help='Path to train_image_paths.csv')
+    parser.add_argument('--test_path', default='./MURA-v1.1/valid_labeled_studies.csv',
+                        help='Path to valid_labeled_studies.csv')
+    parser.add_argument('--test_images', default='./MURA-v1.1/valid_image_paths.csv',
+                        help='Path to valid_image_paths.csv')
+    parser.add_argument('--model_path',
+                        help='Path to a model to resume or proceed with transfer learning')
+    parser.add_argument('-c', '--client', default=0, type=int,
+                        help='Client to evaluate')
+    parser.add_argument('-e', '--epochs', default=10, type=int,
+                        help='number of epochs to train')
+    parser.add_argument('--section', default='XR_WRIS', type=str,
+                        help='XR_SHOU, XR_HUME, XR_FORE, XR_HAND, XR_ELBO, XR_FING, XR_WRIS')
+    parser.add_argument('-b', '--batch_size', type=int, default=8,
+                        help='batch size')
+    parser.add_argument('--print_summary', action='store_true', default='False',
+                        help='print model\'s summary')
+    args = parser.parse_args()
+
+    starting_epoch = 0
+
+    studies_path = np.asarray(pd.read_csv(
+        args.train_path, delimiter=',', header=None))
+    studies_path = [i for i in studies_path if args.section in i[0]]
+
+    model = generate_model(int(args.stage))
+
+    if args.resume is True or args.stage >= 2:
+        if args.resume is True:
+            starting_epoch = int(args.model_path[25:28])
+            print("starting epoch: ", starting_epoch)
+
+    weights = {
+        "XR_SHOU": [0, 0],
+        "XR_HUME": [0, 0],
+        "XR_FORE": [0, 0],
+        "XR_HAND": [0, 0],
+        "XR_ELBO": [0, 0],
+        "XR_FING": [0, 0],
+        "XR_WRIS": [0, 0]
+    }
+
+    paths_imgs = np.loadtxt(args.train_images, dtype='str')
+    for path in paths_imgs:
+        section = path[16:23]
+        if "positive" in path:
+            weights[section][1] += 1
+        elif "negative" in path:
+            weights[section][0] += 1
+
+    for section in weights:
+        weights[section] = weights[section]/np.sum(weights[section])
+
+    # print(weights)
+
+    if args.stage < 3:
+
+        train_paths, val_paths = train_test_split(studies_path)
+
+        train_generator = MuraGenerator(train_paths, batch_size=args.batch_size, weights=weights,
+                                        augment=True if args.stage > 0 else False)
+        val_generator = MuraGenerator(val_paths, batch_size=args.batch_size, weights=weights)
+
+        if not os.path.isdir('logs'):
+            os.mkdir('logs')
+        if not os.path.isdir('models'):
+            os.mkdir('models')
+
+        checkpoint_path = 'models/stage-{}-'.format(
+            args.stage) + 'modeldef-e{epoch:03d}.hdf5'
+        csvlogger = CSVLogger(
+            'logs/defstage-{}-{}.log'.format(args.stage, args.section), append=args.resume)
+        checkpointer = ModelCheckpoint(checkpoint_path, save_best_only=False,
+                                       save_weights_only=True)
+        reduce_lr = ReduceLROnPlateau(monitor='val_binary_accuracy', factor=0.1,
+                                      verbose=1, patience=1, min_lr=1e-7)
+
+        model.fit_generator(train_generator,
+                            callbacks=[csvlogger, checkpointer],
+                            epochs=args.epochs,
+                            initial_epoch=starting_epoch,
+                            validation_data=val_generator)
+
+    elif args.stage == 3:
+        studies_path = np.asarray(pd.read_csv(args.test_path, delimiter=',', header=None))
+        if args.section is not None:
+            studies_path = [path for path in studies_path if args.section in path[0]]
+
+
+        y_pred = []
+        y_true = []
+        y_pred_bin = []
+        sample_w = []
+        for study in studies_path:
+            section = study[0][16:23]
+            img_paths = glob(str(study[0]) + '*.png')
+            images = []
+            for img_path in img_paths:
+                img = image.load_img(img_path, color_mode='rgb',
+                                     target_size=(320, 320))
+
+                img = image.img_to_array(img)
+
+                img = transform_image(img, False)
+                images.append(img)
+
+            images = np.asarray(images)
+            results = model.predict_on_batch(images)
+            y_pred.append(np.mean(results))
+
+            y_true.append(int(study[1]))
+            sample_w.append(weights[section][int(study[1])])
+        print("len y_pred: ", len(y_pred))
+        print("y_pred[0]: ", y_pred[0])
+        y_pred_bin = [0 if i <= 0.5 else 1 for i in y_pred]
+        ones = 0
+        zeroes = 0
+        for pred in y_pred_bin:
+            if int(pred) is 1:
+                ones += 1
+            if int(pred) is 0:
+                zeroes += 1
+        print("Ones: ", ones)
+        print("Zeroes: ", zeroes)
+
+        print("Scores: ")
+        print("Number of studies: ", len(y_true))
+        print("\tLoss: ", log_loss(y_true, y_pred))
+        print("\tLoss (weights): ", log_loss(
+            y_true, y_pred, sample_weight=sample_w))
+        print("\tAccuracy: ", accuracy_score(y_true, y_pred_bin))
+        print("\tAccuracy (weights): ", accuracy_score(
+            y_true, y_pred_bin, sample_weight=sample_w))
+        print("\tRecall: ", recall_score(y_true, y_pred_bin))
+        print("\tRecall (weights): ", recall_score(
+            y_true, y_pred_bin, sample_weight=sample_w))
+        print("\tPrecision: ", precision_score(y_true, y_pred_bin))
+        print("\tPrecision (weights): ", precision_score(
+            y_true, y_pred_bin, sample_weight=sample_w))
+        print("\tAUC: ", roc_auc_score(y_true, y_pred))
+        print("\tAUC (weights): ", roc_auc_score(
+            y_true, y_pred, sample_weight=sample_w))
+
+    elif args.stage == 4:
+        img_paths = np.loadtxt(args.test_path, dtype='str')
+        client = args.client.zfill(5)
+        img_paths = [str(path) for path in img_paths if str(client) in path]
+        if len(img_paths) == 0:
+            print('patient not found on validation set')
+            exit()
+
+        images = []
+        y_pred = []
+        for path in img_paths:
+            img = image.load_img(path, color_mode='rgb',
+                                 target_size=(320, 320))
+            img = image.img_to_array(img)
+            img = np.expand_dims(transform_image(img, False), 0)
+
+            y_pred.append(model.predict(img))
+            images.append(img)
+
+        y_true = 1 if "positive" in img_paths[0] else 0
+        # print(y_pred)
+        # y_pred = np.mean(y_pred) # Ensemble the results
+
+        print("Results: ")
+        print("\ty_true: ", y_true)
+        print("\ty_pred: ", np.mean(y_pred))
+
+        for i in range(len(images)):
+            predicted = 0 if np.squeeze(y_pred[i]) < weights[0] else 1
+
+            plt.subplot(1, 2, 1)
+            plt.imshow(images[i].squeeze())
+            plt.title('Original Image ' + str(y_pred[i]))
+
+            plt.subplot(1, 2, 2)
+            plt.imshow(images[i].squeeze(), 'gray')
+            plt.imshow(get_cam(model, images[i], predicted), 'jet', alpha=0.3)
+            plt.title('Last Layer')
+
+        plt.savefig('logs/map.png')
